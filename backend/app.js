c5eb8647b18daab3b4a515870f55f2aba4296208
DanielXV <daniel.xu@epita.fr>
2025-01-12 16:07:52 +0100

    start

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+//Libraries
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+//server configuration
+var basePath = '/todo';
+var port = 6200;
+
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://mongodb';
+
+// Connection to DB
+mongoose.connect(MONGO_URL)
+    .then(() => {
+      console.log('Backend Started');
+    })
+    .catch(err => {
+        console.error('Backend error:', err.stack);
+        process.exit(1);
+    });
+
+// Routes and Backend Funcioncalities
+var todoListRoutes = require('./src/routes/todoListRoutes');
+
+// App Instance
+var app = express();
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(basePath, todoListRoutes);
+
+// Execute App
+app.listen(port, () => {
+  console.log('TodoList Backend running on Port: ',port);
+});
