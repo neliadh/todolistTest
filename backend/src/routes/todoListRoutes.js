819bb5ca3f11875e4dded1ab9000d7e2ee0d0dce
DanielXV <daniel.xu@epita.fr>
2025-01-12 21:14:23 +0100

    dsqdsqdd

--- backend/src/routes/todoListRoutes.js
+++ backend/src/routes/todoListRoutes.js
@@ -3,65 +3,65 @@ var app = express();
 var router = express.Router();
 
 //Schema
-var TodoList = require('../models/TodoList');
+// var TodoList = require('../models/TodoList');
 
-// Get Specific
-router.route('/:id').get(function (req, res) {
-  var id = req.params.id;
-  TodoList.findById(id, function (err, item){
-      res.json(item);
-  });
-});
+// // Get Specific
+// router.route('/:id').get(function (req, res) {
+//   var id = req.params.id;
+//   TodoList.findById(id, function (err, item){
+//       res.json(item);
+//   });
+// });
 
-// Get All Items
-router.route('/').get(function (req, res) {
-  TodoList.find(function (err, items){
-    if(err){
-      console.log(err);
-    } else {
-      res.json(items);
-    }
-  });
-});
+// // Get All Items
+// router.route('/').get(function (req, res) {
+//   TodoList.find(function (err, items){
+//     if(err){
+//       console.log(err);
+//     } else {
+//       res.json(items);
+//     }
+//   });
+// });
 
-// Add item
-router.route('/add').post(function (req, res) {
-  var item = new TodoList(req.body);
-      item.save()
-    .then(item => {
-    res.json('Added');
-    })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
-    });
-});
+// // Add item
+// router.route('/add').post(function (req, res) {
+//   var item = new TodoList(req.body);
+//       item.save()
+//     .then(item => {
+//     res.json('Added');
+//     })
+//     .catch(err => {
+//     res.status(400).send("unable to save to database");
+//     });
+// });
 
-//  Update Specific
-router.route('/update/:id').post(function (req, res) {
-  TodoList.findById(req.params.id, function(err, item) {
-    if (!item)
-      return next(new Error('Could not load Document'));
-    else {
-      item.desc = req.body.desc;
+// //  Update Specific
+// router.route('/update/:id').post(function (req, res) {
+//   TodoList.findById(req.params.id, function(err, item) {
+//     if (!item)
+//       return next(new Error('Could not load Document'));
+//     else {
+//       item.desc = req.body.desc;
 
-      item.save().then(item => {
-          res.json('Updated');
-      })
-      .catch(err => {
-            res.status(400).send("unable to update the database");
-      });
-    }
-  });
-});
+//       item.save().then(item => {
+//           res.json('Updated');
+//       })
+//       .catch(err => {
+//             res.status(400).send("unable to update the database");
+//       });
+//     }
+//   });
+// });
 
-// Delete Specific
-router.route('/delete/:id').get(function (req, res) {
-  TodoList.findByIdAndRemove({_id: req.params.id},
-       function(err, item){
-        if(err) res.json(err);
-        else res.json('Deleted');
-    });
-});
+// // Delete Specific
+// router.route('/delete/:id').get(function (req, res) {
+//   TodoList.findByIdAndRemove({_id: req.params.id},
+//        function(err, item){
+//         if(err) res.json(err);
+//         else res.json('Deleted');
+//     });
+// });
 
 router.route('/health').get(function (req, res) {
   res.json('Healthy');
